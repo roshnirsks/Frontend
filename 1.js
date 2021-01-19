2f5797471671a912d3cccd36babab01e152168c5
roshnirsks <kumariroshni81998@gmail.com>
2021-01-19 18:29:36 +0530

    js file added

---
+++ 1.js
@@ -0,0 +1,31 @@
+const list = document.querySelector('https://s3-ap-southeast-1.amazonaws.com/he-public-data/books8f8fe52.json');
+
+let item = [];
+let bill = [];
+
+function addToCart(x) {
+    const span = `<span class="price">$${products[x].price}</span>`
+    const result = products[x].name + " " + span;
+    if (item.includes(products[x]) === false) item.push(result);
+    bill.push(products[x].price);
+
+}
+
+function cart() {
+    list.innerHTML = "";
+    for (let i = 0; i < item.length; i++) {
+        let text = item[i];
+        let listItem = document.createElement("LI");
+        listItem.innerHTML = text;
+        list.appendChild(listItem);
+        listItem.style.textAlign = 'left';
+    }
+    const btn = document.querySelector('.bill');
+    const total = bill.reduce(add);
+
+    function add(accumulator, a) {
+        return parseInt(accumulator) + parseInt(a);
+    }
+    console.log(total);
+    btn.innerHTML = `Total Bill is $${total}`;
+}
